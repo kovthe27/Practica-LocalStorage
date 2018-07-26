d78abf6aa4db703bde087d2529184b4287e5a958
kovthe27 <39032531+kovthe27@users.noreply.github.com>
2018-07-26 17:40:50 -0600

    Ejercicio 2

--- semana12/public/js/apartamentos/controlador_apartamentos.js
+++ semana12/public/js/apartamentos/controlador_apartamentos.js
@@ -8,11 +8,12 @@ let outputIndice = document.querySelector('#txtIndice');
 const inputNombre = document.querySelector('#txtNombre');
 const inputAnimal = document.querySelector('#txtAnimal');
 const inputEdad = document.querySelector('#txtEdad');
-
-
+const inputID1 = document.querySelector("#txtId1");
+const inputTipo = document.querySelector("#txtAnimal");
+const botonRegistrar = document.querySelector("#btnRegistrar");
 
 botonBuscar.addEventListener('click', obtenerDatos);
-
+botonRegistrar.addEventListener('click', agregarMascota);
 
 function obtenerDatos(){
     let nId = inputId.value;
@@ -23,4 +24,23 @@ function obtenerDatos(){
 
 };
 
-function agregarMascota (){}
+function agregarMascota (){
+    let nId1= inputID1.value;
+    let bExiste=0;
+    bExiste=determinarPosicion(nId1);
+    let nuevoAnimal=[];
+
+    let id=inputID1.value;
+    let nNombre=inputNombre.value;
+    let sTipo=inputAnimal.value;
+    let nEdad=Number(inputEdad.value);
+
+    if(bExiste!=-1){
+        window.alert("Ya existe")
+    }
+    else{
+        nuevoAnimal.push(id,nNombre,sTipo,nEdad);
+        registrarMascota();
+    }
+}
+
--- semana12/public/js/apartamentos/servicio_apartamentos.js
+++ semana12/public/js/apartamentos/servicio_apartamentos.js
@@ -1,20 +1,19 @@
 'use strict';
-const mMascotas= [
-    ['M01', 'Athos', 'Gato', 1],
-    ['M02', 'Tsunade', 'Gato', 2],
-    ['M03', 'Aramis', 'Gato', 3],
-    ['M04', 'Portos ', 'Gato', 1],
-    ['M05', 'Navaroog', 'Dragón', 150],
-    ['M06', 'Sphinx ', 'Dragón', 75],
-    ['M07', 'Alaska', 'Dragón', 49],
-    ['M08', 'Jack', 'Perro ', 8],
-    ['M09', 'Nakako', 'Perro ', 4]
+// const mMascotas= [
+//     ['M01', 'Athos', 'Gato', 1],
+//     ['M02', 'Tsunade', 'Gato', 2],
+//     ['M03', 'Aramis', 'Gato', 3],
+//     ['M04', 'Portos ', 'Gato', 1],
+//     ['M05', 'Navaroog', 'Dragón', 150],
+//     ['M06', 'Sphinx ', 'Dragón', 75],
+//     ['M07', 'Alaska', 'Dragón', 49],
+//     ['M08', 'Jack', 'Perro ', 8],
+//     ['M09', 'Nakako', 'Perro ', 4]
     
-];
+// ];
 
 function obtenerListaMascotas(){
-    return mMascotas;
-    localStorage.setItem('mMascotasLS', JSON.stringify(mMascotas));
+    let mMascotas=JSON.parse(localStorage.getItem("mMascotasLS"));
 
     if(mMascotas == null){
         mMascotas= [
@@ -36,11 +35,18 @@ function obtenerListaMascotas(){
 
 function determinarPosicion(pnId){
     let nPosicion = -1; 
-    for(let i = 0; i < mMascotas.length; i++){
+    let mMascotas= obtenerListaMascotas();
+    for(let i = 0; i < obtenerListaMascotas().length; i++){
         if(mMascotas[i][0] == pnId){
             nPosicion = i;
         }
     }
     return nPosicion;
-}
+};
 
+function registrarMascota(pmMascota){
+    let mMascota = obtenerListaMascotas();
+    mMascota.push(pmMascota);
+
+    localStorage.setItem('mMascotasLS', JSON.stringify(mMascota));
+};
